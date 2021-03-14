c60c1e4cd0afd6f0256750434e2b63ee084fc359
Remi Hinschberger-Bocher <rhbocher@enseirb-matmeca.fr>
2021-03-14 21:02:20 +0100

    pas de timer ni affichege bomb (remplacé par arbre) mais gere les zones de dégats des bombes

--- sources/src/bomb.c
+++ sources/src/bomb.c
@@ -7,26 +7,24 @@
 #include <misc.h>
 #include <constant.h>
 
+
 struct bomb {
 	int x, y;
-	int range;
+	int area;
 };
 
-struct bomb* bomb_init(int range) {
+struct bomb* bomb_init(int area, struct player * player) {
 	struct bomb* bomb = malloc(sizeof(*bomb));
 	if (!bomb)
 		error("Memory error");
-	bomb->range=range;
+
+	bomb->area = area;
+	bomb->x = player_get_x(player);
+	bomb->y = player_get_y(player);
 	return bomb;
 }
 
 
-void bomb_set_position(struct player *player,struct bomb *bomb) {
-	assert(player);
-	assert(bomb);
-	bomb->x = player_get_x(player);
-	bomb->y = player_get_y(player);
-}
 
 
 void bomb_free(struct bomb * bomb) {
@@ -36,20 +34,12 @@ void bomb_free(struct bomb * bomb) {
 
 
 
-void bomb_display(struct bomb * bomb) {
-	assert(bomb);
-	window_display_image(sprite_get_bomb(),
-			bomb->x * SIZE_BLOC, bomb->y * SIZE_BLOC);
-}
-
-
-
-
 
 ///////////////// deposition de bombe pas encore finie
 void put_bomb(struct player* player,struct map* map)
 {
-	//struct bomb * bomb =bomb_init(1); ///////////////////// passer le 1 en parametre
+	
+	struct bomb * bomb =bomb_init(2,player); ///////////////////// passer le 1 en parametre
 	if (player_get_nb_bomb(player)==0)
 	{
 		return ;
@@ -57,8 +47,18 @@ void put_bomb(struct player* player,struct map* map)
 	else
 	{
 		player_dec_nb_bomb(player);
-		//bomb_display(bomb);
-		map_set_cell_type(map,player_get_x(player) , player_get_y(player), CELL_BOMB);
+		map_set_cell_type(map,bomb->x , bomb->y, CELL_TREE);
+		
+		//mettre les differentes bombes
+		///explosion
+		while (bomb->area>=0){
+			map_set_cell_type(map,bomb->x+(bomb->area) , bomb->y, CELL_TREE);
+			map_set_cell_type(map,bomb->x , bomb->y+(bomb->area), CELL_TREE);
+			map_set_cell_type(map,bomb->x-(bomb->area) , bomb->y, CELL_TREE);
+			map_set_cell_type(map,bomb->x , bomb->y-(bomb->area), CELL_TREE);
+			bomb->area-=1;
+		}
+		
 		return ;
 	}
 
