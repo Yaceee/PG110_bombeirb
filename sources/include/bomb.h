b0ab7749afb173d5a3638f7dbc776f2444382026
Remi Hinschberger-Bocher <rhbocher@enseirb-matmeca.fr>
2021-03-14 21:03:54 +0100

    MAJ des changement de bomb.c

--- sources/include/bomb.h
+++ sources/include/bomb.h
@@ -14,11 +14,7 @@ struct bomb* bomb_init(int range);
 void   bomb_free(struct bomb *bomb);
 
 // Set the position of the bomb
-void bomb_set_position(struct player *player,struct bomb *bomb);
 
-
-// Display the bomb on the screen
-void bomb_display(struct bomb* bomb);
 void put_bomb(struct player* player,struct map* map);
 
 #endif /* PLAYER_H_ */
