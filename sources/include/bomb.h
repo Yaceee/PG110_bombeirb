1340459ebc5ba6effa4f4017c51db21280f09965
Remi Hinschberger-Bocher <rhbocher@enseirb-matmeca.fr>
2021-03-17 19:57:28 +0100

    MAJ suite aux modification de bomb.c qui gere completement les bombes

--- sources/include/bomb.h
+++ sources/include/bomb.h
@@ -5,6 +5,7 @@
 #include <constant.h>
 #include <player.h>
 #include <game.h>
+#include <game.h>
 
 
 struct bomb;
@@ -13,9 +14,15 @@ struct bomb;
 struct bomb* bomb_init(int range);
 void   bomb_free(struct bomb *bomb);
 
+//change l'etat des bombes
+void bomb_display(struct map * map);
+
 //gere les explosions
 int condition_explosion(struct map * map,int x ,int y);
-void explosion(struct bomb * bomb, struct map * map);
+void explosion(struct bomb * bomb, struct map * map, enum cell_type CELL);
+struct bomb* bomb_init_for_tab();
+void initialisation_tab_bomb();
+
 
 //main
 void put_bomb(struct player* player,struct map* map);
